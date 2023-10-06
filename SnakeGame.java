6c4a7cf3728d41f4fab3e83e88a55ab81892efd0
Evelio Velez <114781973+evelez0101@users.noreply.github.com>
2023-10-06 14:21:07 -0400

    Update SnakeGame.java

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,5 +1,10 @@
 public class SnakeGame 
 {
+    // This project was made possible by Bro Code's Snake Game tutorial.
+    // This project was heavily based on his project and built upon as 
+    // well as descriptive comments were added to break down the functions the code serves.  
+    // Link to the video: https://youtu.be/bI6e6qjJ8JQ?si=YR76j9IBoNYth-iP
+    
     public static void main(String [] args)
     {
         // Creates an instance of our Game Frame
